d741d6bcde1ebc5634db5ccfb9594f028a51bbe6
Ritika_chaudhary <40438594+RitikaChaudhary15@users.noreply.github.com>
2019-09-22 00:04:03 +0530

    Update Caching-the-inverse-of-matrix.R

--- Caching-the-inverse-of-matrix.R
+++ Caching-the-inverse-of-matrix.R
@@ -1,3 +1,16 @@
+## Usually creating a matrix inversion is computationally costly
+## In order speed it up, the process can be cached
+## In R this is used by scoping rules using the '<<-' symbol
+## This sybol when used inside a function creates a 
+## special object. 
+## I show the use of caching to create a special matrix object
+## and cache its inverse using the two following functions:
+
+
+
+## First function: creates a special 'matrix' which is represented
+## in the R environment as a list and then cache its inverse
+
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
@@ -12,7 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
          setInverse = setInverse,
          getInverse = getInverse)
 }
-
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
 ## Write a short comment describing this function
 
